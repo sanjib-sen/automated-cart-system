b96c5df2ce9f1043bfa22787a4b23d36224857ce
Sanjib Kumar Sen <sksenonline@gmail.com>
2021-05-03 13:56:23 +0600

    removed forgetpass and rememberpass from admin login

--- pages/index.php
+++ pages/index.php
@@ -93,27 +93,6 @@
                         <label class="form-label" for="form1Example2">Password</label>
                     </div>
 
-                    <!-- 2 column grid layout for inline styling -->
-                    <div class="row mb-4">
-                        <div class="col d-flex justify-content-center">
-                            <!-- Checkbox -->
-                            <div class="form-check">
-                                <input
-                                        class="form-check-input"
-                                        type="checkbox"
-                                        value=""
-                                        id="form1Example3"
-                                        checked
-                                />
-                                <label class="form-check-label" for="form1Example3"> Remember me </label>
-                            </div>
-                        </div>
-
-                        <div class="col">
-                            <!-- Simple link -->
-                            <a href="#!">Forgot password?</a>
-                        </div>
-                    </div>
 
                     <!-- Submit button -->
                     <button type="submit" class="btn btn-primary btn-block">Sign in</button>
